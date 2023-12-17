59611cde7612f56083b5bca9c290be8315e5943e
ddileep01 <dileep18052001@gmail.com>
2023-12-17 16:00:35 +0530

    feat: updated hero section

--- components/navbar.js
+++ components/navbar.js
@@ -31,7 +31,7 @@ const Navbar = () => {
 
                 <Disclosure.Button
                   aria-label="Toggle Menu"
-                  className="px-2 py-1 ml-auto text-gray-500 rounded-md lg:hidden hover:text-green-500 focus:text-green-500 focus:bg-green-100 focus:outline-none dark:text-gray-300 dark:focus:bg-trueGray-700"
+                  className="px-2 py-1 ml-auto text-gray-500 rounded-md lg:hidden hover:text-green-950 focus:text-green-950 focus:bg-green-100 focus:outline-none dark:text-gray-300 dark:focus:bg-trueGray-700"
                 >
                   <svg
                     className="w-6 h-6 fill-current"
@@ -60,7 +60,7 @@ const Navbar = () => {
                       <Link
                         key={index}
                         href="/"
-                        className="w-full px-4 py-2 -ml-4 text-gray-500 rounded-md dark:text-gray-300 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 dark:focus:bg-gray-800 focus:outline-none"
+                        className="w-full px-4 py-2 -ml-4 text-gray-500 rounded-md dark:text-gray-300 hover:text-green-950 focus:text-green-950 focus:bg-green-100 dark:focus:bg-gray-800 focus:outline-none"
                       >
                         {item}
                       </Link>
@@ -85,7 +85,7 @@ const Navbar = () => {
               <li className="mr-3 nav__item" key={index}>
                 <Link
                   href="/"
-                  className="inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline rounded-md dark:text-gray-200 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-gray-800"
+                  className="inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline rounded-md dark:text-gray-200 hover:text-green-950 focus:text-green-950 focus:bg-green-100 focus:outline-none dark:focus:bg-gray-800"
                 >
                   {menu}
                 </Link>
--- pages/index.js
+++ pages/index.js
@@ -16,11 +16,8 @@ const Home = () => {
   return (
     <>
       <Head>
-        <title>Nextly - Free Nextjs & TailwindCSS Landing Page Template</title>
-        <meta
-          name="description"
-          content="Nextly is a free landing page template built with next.js & Tailwind CSS"
-        />
+        <title>Akhanda Seva Samsthan</title>
+        <meta name="description" content="Akhanda Seva Samsthan" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -28,7 +25,8 @@ const Home = () => {
       <Hero />
       <SectionTitle
         pretitle="Nextly Benefits"
-        title=" Why should you use this landing page">
+        title=" Why should you use this landing page"
+      >
         Nextly is a free landing page & marketing website template for startups
         and indie projects. Its built with Next.js & TailwindCSS. And its
         completely open-source.
@@ -37,7 +35,8 @@ const Home = () => {
       <Benefits imgPos="right" data={benefitTwo} />
       <SectionTitle
         pretitle="Watch a video"
-        title="Learn how to fullfil your needs">
+        title="Learn how to fullfil your needs"
+      >
         This section is to highlight a promo or demo video of your product.
         Analysts says a landing page with video has 3% more conversion rate. So,
         don&apos;t forget to add one. Just like this.
@@ -45,7 +44,8 @@ const Home = () => {
       <Video />
       <SectionTitle
         pretitle="Testimonials"
-        title="Here's what our customers said">
+        title="Here's what our customers said"
+      >
         Testimonails is a great way to increase the brand trust and awareness.
         Use this section to highlight your popular customers.
       </SectionTitle>
@@ -60,6 +60,6 @@ const Home = () => {
       <PopupWidget />
     </>
   );
-}
+};
 
 export default Home;
