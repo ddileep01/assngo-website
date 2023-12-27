b893635dceafe29c3a8ac675c9abf8df69c040c4
ddileep01 <dileep18052001@gmail.com>
2023-12-27 10:21:27 +0530

    feat: added speed insights from vercel

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { ThemeProvider } from "next-themes";
 import "../css/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+      <SpeedInsights />
+    </>
   );
 }
 
