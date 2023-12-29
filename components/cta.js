29ceef6b0662bb0dd0e0133716629afda8fedf41
ddileep01 <dileep18052001@gmail.com>
2023-12-29 19:36:58 +0530

    fix: update CTA action

--- components/cta.js
+++ components/cta.js
@@ -15,7 +15,7 @@ const Cta = () => {
         </div>
         <div className="flex-shrink-0 w-full text-center lg:w-auto">
           <a
-            href="https://github.com/web3templates"
+            href="/donation"
             target="_blank"
             rel="noopener"
             className="inline-block py-3 mx-auto text-lg font-medium text-center text-green-950 bg-white rounded-md px-7 lg:px-10 lg:py-5 "
