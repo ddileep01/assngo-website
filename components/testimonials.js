9c2685dc4c859399ce669cbbd602ab290844c095
ddileep01 <dileep18052001@gmail.com>
2024-03-04 13:58:02 +0530

    feat: updated testimonial card style

--- components/testimonials.js
+++ components/testimonials.js
@@ -1,6 +1,6 @@
-import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import Container from "./container";
+import Image from "next/image";
 
 import userOneImg from "../public/img/user1.jpg";
 import userTwoImg from "../public/img/user2.jpg";
@@ -10,120 +10,72 @@ const Testimonials = () => {
   return (
     <Container>
       <div className="grid gap-10 lg:grid-cols-2 xl:grid-cols-3">
-        <div className="lg:col-span-2 xl:col-auto">
-          <div className="flex flex-col justify-between w-full h-full bg-gray-100 px-14 rounded-2xl py-14 dark:bg-trueGray-800">
-            <p className="leading-normal ">
-              Akhanda Seva Samsthan's dedication to social responsibility and
-              their commitment to various causes make them a valuable asset to
-              the communities they serve. I feel proud to be a part-time
-              volunteer for the organisation.
-            </p>
-
-            <Avatar
-              image={userOneImg}
-              name="Tarak"
-              title="Youth For Seva Organization"
-            />
-          </div>
-        </div>
-        <div className="">
-          <div className="flex flex-col justify-between w-full h-full bg-gray-100 px-14 rounded-2xl py-14 dark:bg-trueGray-800">
-            <p className="leading-normal ">
-              Proud of Akhanda Seva Samsthan's dedication to education and
-              healthcare. They're a beacon of hope, offering aid to the needy
-              and transforming lives. Actions speak louder than words; their
-              work speaks volumes.
-            </p>
-
-            <Avatar
-              image={userTwoImg}
-              name="Vishnu Priyanka"
-              title="Assistant Professor"
-            />
-          </div>
-        </div>
-        <div className="">
-          <div className="flex flex-col justify-between w-full h-full bg-gray-100 px-14 rounded-2xl py-14 dark:bg-trueGray-800">
-            <p className="leading-normal ">
-              Grateful for the chance to aid Akanda Seva Samithi's food donation
-              efforts. Their dedication is inspiring. Let's join hands to make a
-              difference. Thank you, Akanda Seva Samithi, for this meaningful
-              opportunity to serve our community together.
-            </p>
-
-            <Avatar
-              image={userThreeImg}
-              name="Pavan Kachala"
-              title="Software Engineer"
-            />
-          </div>
-        </div>
-        <div className="">
-          <div className="flex flex-col justify-between w-full h-full bg-gray-100 px-14 rounded-2xl py-14 dark:bg-trueGray-800">
-            <p className="leading-normal ">
-              As a local healthcare provider, collaborating with Akhanda Seva
-              Samsthan has allowed us to extend our services to underserved
-              communities. Together, we've been able to create a healthier and
-              happier community.
-            </p>
-
-            <Avatar
-              image={userTwoImg}
-              name="Dylan Ambrose"
-              title="Lead marketer at Netflix"
-            />
-          </div>
-        </div>
-        <div className="">
-          <div className="flex flex-col justify-between w-full h-full bg-gray-100 px-14 rounded-2xl py-14 dark:bg-trueGray-800">
-            <p className="leading-normal ">
-              Attending Akhanda Seva Samsthan's events has been an enlightening
-              experience. The organization creates spaces for meaningful
-              discussions and fosters a sense of community that extends beyond
-              the event itself. I look forward to future engagements.
-            </p>
-
-            <Avatar
-              image={userTwoImg}
-              name="Dylan Ambrose"
-              title="Lead marketer at Netflix"
-            />
-          </div>
-        </div>
+        <TestimonialCard
+          image={userOneImg}
+          name="Tarak"
+          title="Youth For Seva Organization"
+          text="Akhanda Seva Samsthan's dedication to social responsibility and their commitment to various causes make them a valuable asset to the communities they serve. I feel proud to be a part-time volunteer for the organisation."
+        />
+        <TestimonialCard
+          image={userTwoImg}
+          name="Vishnu Priyanka"
+          title="Assistant Professor"
+          text="When I first heard about Akhanda Seva Samsthan I felt proud about the team,who started the NGO. Where the team constantly trying to bring a positive change towards education and health care. The organization is working and always been a helping hand to a lot of needy persons and also making a lot of difference in their lives. A helping hand is always better than thousands of helping words."
+        />
+        <TestimonialCard
+          image={userThreeImg}
+          name="Pavan Kachala"
+          title="Software Engineer"
+          text="I am incredibly grateful to have had the opportunity to donate food through Akhanda Seve Samsthan. Knowing that every rupee I contributed went directly towards helping others brought me immense joy and satisfaction. The selfless dedication of the people involved in this NGO is truly inspiring. They work tirelessly to improve the lives of those in need, and their commitment to the betterment of society is commendable. I wholeheartedly support their noble cause and encourage others to join in their efforts to make a positive impact on our community. Thank you, Akanda Seva Samithi, for your invaluable service and for giving me the chance to be a part of something meaningful."
+        />
+        <TestimonialCard
+          image={userThreeImg}
+          name="Hema"
+          title="Software Engineer"
+          text="I am incredibly grateful to have had the opportunity to donate food through Akhanda Seve Samsthan. Knowing that every rupee I contributed went directly towards helping others brought me immense joy and satisfaction. The selfless dedication of the people involved in this NGO is truly inspiring. They work tirelessly to improve the lives of those in need, and their commitment to the betterment of society is commendable. I wholeheartedly support their noble cause and encourage others to join in their efforts to make a positive impact on our community. Thank you, Akanda Seva Samithi, for your invaluable service and for giving me the chance to be a part of something meaningful."
+        />
       </div>
     </Container>
   );
 };
 
-function Avatar(props) {
+const TestimonialCard = ({ image, name, title, text }) => {
+  const [expanded, setExpanded] = useState(false);
+
+  const toggleExpansion = () => {
+    setExpanded(!expanded);
+  };
+
   return (
-    <div className="flex items-center mt-8 space-x-3">
-      <div className="flex-shrink-0 overflow-hidden rounded-full w-14 h-14">
-        <Image
-          src={props.image}
-          width="40"
-          height="40"
-          alt="Avatar"
-          placeholder="blur"
-        />
-      </div>
-      <div>
-        <div className="text-lg font-medium">{props.name}</div>
-        <div className="text-gray-600 dark:text-gray-400">{props.title}</div>
+    <div className="lg:col-span-2 xl:col-auto">
+      <div className="flex flex-col justify-between w-full h-full bg-gray-100 px-14 rounded-2xl py-14 dark:bg-trueGray-800">
+        <p className="leading-normal">
+          {expanded ? text : `${text.slice(0, 100)}...`}
+          <button
+            onClick={toggleExpansion}
+            className="text-blue-500 mt-2 focus:outline-none"
+          >
+            {expanded ? "Read less" : "Read more"}
+          </button>
+        </p>
+        <Avatar image={image} name={name} title={title} />
       </div>
     </div>
   );
-}
+};
 
-function Mark(props) {
+const Avatar = ({ image, name, title }) => {
   return (
-    <>
-      {" "}
-      <mark className="text-indigo-800 bg-indigo-100 rounded-md ring-indigo-100 ring-4 dark:ring-indigo-900 dark:bg-indigo-900 dark:text-indigo-200">
-        {props.children}
-      </mark>{" "}
-    </>
+    <div className="flex items-center mt-8 space-x-3">
+      <div className="flex-shrink-0 overflow-hidden rounded-full w-14 h-14">
+        <Image src={image} alt="Avatar" width={40} height={40} />
+      </div>
+      <div>
+        <div className="text-lg font-medium">{name}</div>
+        <div className="text-gray-600 dark:text-gray-400">{title}</div>
+      </div>
+    </div>
   );
-}
+};
 
 export default Testimonials;
