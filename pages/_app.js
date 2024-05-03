4e576fed3722d8e66e054c9b78b7afbdf73c3db0
ddileep01 <dileep18052001@gmail.com>
2024-05-03 11:01:27 +0530

    feat: added Web Analytics (vercel)

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 // import { ThemeProvider } from "next-themes";
 import "../css/tailwind.css";
 import "../css/global.css";
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       {/* <ThemeProvider attribute="class"> */}
         <Component {...pageProps} />
+        <Analytics />
       {/* </ThemeProvider> */}
     </>
   );
